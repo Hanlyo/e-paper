bbd79e54dbc035cf6c9afa8613a6cf1336c2be96
Hanlyo <walner.martin@web.de>
2025-03-24 20:59:08 +0100

    test

--- RaspberryPi_JetsonNano/c/examples/EPD_7in3f_test.c
+++ RaspberryPi_JetsonNano/c/examples/EPD_7in3f_test.c
@@ -629,11 +629,14 @@ int EPD_7in3f_test(void)
         return 1;
     }
 
+    print("array geholt");
+
     // String holen
     cJSON *temp_value = get_value_from_json_array(json_array, 1, "humidity");
     if (cJSON_IsString(temp_value)) {
         printf("Luftfeuchtigkeit am zweiten Tag: %s\n", temp_value->valuestring);
     }
+    print("nach humidity");
 
     // JSON-Objekt holen
     cJSON *details = get_value_from_json_array(json_array, 1, "temp");
@@ -643,6 +646,8 @@ int EPD_7in3f_test(void)
             printf("Temperatur am zweiten Tag: %s\n", humidity->valuestring);
         }
     }
+
+    print("nach temp");
 
 
     cJSON_Delete(json_array); // Speicherfreigabe
