773c2598e688471947ec93e4b751089ea2b713aa
Hanlyo <walner.martin@web.de>
2025-03-28 16:28:53 +0100

    test

--- RaspberryPi_JetsonNano/c/examples/EPD_7in3f_test.c
+++ RaspberryPi_JetsonNano/c/examples/EPD_7in3f_test.c
@@ -552,8 +552,8 @@ int EPD_7in3f_test(void)
             // test etwas unter dem aktuellen Tag darstellen
             // font12: 12 Pixel hoch und 7 Pixel breit
             // 90 Pixel pro Tag ^= 14 Zeichen
-            Paint_DrawString_EN(x-45, y+24, "Staub saugen", &Font12, EPD_7IN3F_WHITE, EPD_7IN3F_ORANGE);
-            Paint_DrawString_EN(x-45, y+32, "Spuelen", &Font12, EPD_7IN3F_WHITE, EPD_7IN3F_ORANGE);
+            Paint_DrawString_EN(x-42, y+24, "Staub saugen", &Font12, EPD_7IN3F_WHITE, EPD_7IN3F_ORANGE);
+            Paint_DrawString_EN(x-42, y+36, "Spuelen", &Font12, EPD_7IN3F_WHITE, EPD_7IN3F_ORANGE);
         } else {
             Paint_DrawString_EN(x, y, numStr, &Font24, EPD_7IN3F_WHITE, EPD_7IN3F_BLACK);
         }
